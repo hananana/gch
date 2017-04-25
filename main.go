7081fb20e29e3155540b2f8388cdeb878c023d3b
hananana <hananana.vimmer@gmail.com>
2017-04-25 17:02:30 +0900

    :hammer: get repos

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/urfave/cli"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("hoge")
+		repos, err := repos()
+		if err != nil {
+			return err
+		}
+
+		return nil
 	}
 	app.Run(os.Args)
+}
 
+func repos() ([]string, error) {
+	out, err := exec.Command("ghq", "list").Output()
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(out)))
+	lines := make([]string, 0, 100)
+	for scanner.Scan() {
+		append(lines, scanner.Text())
+	}
+
+	return lines, nil
 }
